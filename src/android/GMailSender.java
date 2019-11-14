65004f20d701fcbd1a9b71310f2566454c2c7c7e
Francois du Plessis <f@elifestyle.co.za>
2019-11-14 18:27:25 +0200

    Java File import

--- src/android/GMailSender.java
+++ src/android/GMailSender.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.File;
 import java.security.Security;
 import java.util.Properties;
 
